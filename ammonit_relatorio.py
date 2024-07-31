920cc048e8217511456c38ef8a491faba55fccb0
João Vítor <joovitor177@gmail.com>
2024-07-31 16:25:56 -0300

    versão 01 do relatorio diário do MCTI

---
+++ ammonit_relatorio.py
@@ -0,0 +1,137 @@
+import pandas as pd
+from io import StringIO
+from datetime import datetime
+import numpy as np
+import logging
+
+# Dado um .csv de minuto, retorna o dataframe de dados e o dataframe de metadados
+def parser(filename):
+    dados = []
+    metadados = []
+    with open(filename, 'r') as arquivo:
+        linhas = arquivo.readlines()
+
+    metadados_encontrados = False
+
+    for linha in linhas:
+        if linha.startswith("[System]"):
+            metadados_encontrados = True
+        if metadados_encontrados:
+            metadados.append(linha)
+        else:
+            dados.append(linha)
+    return dados, metadados
+
+def lista_para_df(lista):
+    return pd.read_csv(StringIO(''.join(lista)))
+
+def calcular_dia_juliano(timestamp_str):
+    timestamp = datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S')
+    return timestamp.timetuple().tm_yday
+
+def timestamp_para_horalocal(timestamp_str):
+    timestamp = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S")
+    horalocal = timestamp.hour * 60 + timestamp.minute
+    return horalocal
+
+def etl_minute(df):
+    duplicated_rows = df[df.duplicated()]
+    
+    df['Date/time'] = pd.to_datetime(df['Date/time'])
+    start_timestamp = df["Date/time"].min()
+    end_timestamp = df["Date/time"].max()
+    all_timestamps = pd.date_range(start=start_timestamp, end=end_timestamp, freq='min')
+
+    missing_timestamps = all_timestamps.difference(df['Date/time'])
+
+    # Automatizar isso
+    latitude = -0.05398
+    longitude = -51.15508
+    longitude_ref = -45
+    isc = 1367
+    colunas = df.columns
+    ghi_avg_colunas = [coluna for coluna in colunas if 'GHI' in coluna and coluna.endswith('Avg')]
+    info_ghi = {col: {'contador_fisicamente_possivel': 0} for col in ghi_avg_colunas}
+
+    for index, row in df.iterrows():
+        timestamp_str = str(row['Date/time'])
+        dia_juliano = calcular_dia_juliano(timestamp_str)
+
+        rad = (2 * np.pi * (dia_juliano - 1) / 365)
+        eo = 1.00011 + (0.0334221 * np.cos(rad)) + (0.00128 * np.sin(rad)) + (0.000719 * np.cos(2 * rad)) + (0.000077 * np.sin(2 * rad))
+        io = isc * eo
+        iox = max(io, 0)
+
+        horalocal = timestamp_para_horalocal(timestamp_str)
+        et = 229.18 * (0.000075 + 0.001868 * np.cos(rad) - 0.032077 * np.sin(rad) - 0.014615 * np.cos(2 * rad) - 0.04089 * np.sin(2 * rad))
+        horasolar = (horalocal + ((4 * (longitude - longitude_ref)) + et)) / 60
+        omega = (horasolar - 12) * 15
+        declinacao = 23.45 * np.sin(((dia_juliano + 284) * (360 / 365)) * np.pi / 180)
+        cosAZS = (np.sin(latitude * np.pi / 180) * np.sin(declinacao * np.pi / 180)) + (np.cos(latitude * np.pi / 180) * np.cos(declinacao * np.pi / 180) * np.cos(omega * np.pi / 180))
+        AZS = np.arccos(cosAZS) * 180 / np.pi
+
+        cosAZS12 = cosAZS ** 1.2 if AZS <= 90 else 0
+        fpmin = -4
+        fpmax = (1.5 * iox * cosAZS12) + 100
+
+        for col in ghi_avg_colunas:
+            value = row[col]
+            if pd.notna(value) and isinstance(value, (int, float, str)):
+                try:
+                    value = float(value)
+                    if fpmin < value < fpmax:
+                        info_ghi[col]['contador_fisicamente_possivel'] += 1
+                except ValueError:
+                    continue
+
+    return len(duplicated_rows), len(missing_timestamps), info_ghi
+
+# Exemplo de uso
+
+# Passando o csv raw e retornando o relatório por estação
+
+# Tenho que passar de forma automática o Project Name, Serial, Override_latitude e Override_longitude
+def processa_tudo(filename, name_project, data):
+    dados, _ = parser(filename)
+    df = lista_para_df(dados)
+
+    qtd_duplicados, qtd_missing, dic = etl_minute(df)
+
+    with open(f"summary_log.txt-{data}", 'a', encoding="utf-8") as f:
+        f.write(f"Nome do Projeto: {name_project}\n")
+        f.write(f"Data: {data}\n")
+        f.write(f"    Quantidade de duplicados: {qtd_duplicados}\n")
+        f.write(f"    Quantidade de timestamps faltantes: {qtd_missing}\n")
+
+        outer_key = next(iter(dic))
+        inner_dict = dic[outer_key]
+        value = next(iter(inner_dict.values()))
+
+        total = 1440
+
+        f.write(f"    Quantidade fisicamente possíveis: {value}\n")
+
+        anomalos = total - int(value)
+        #status = ""
+        if anomalos/total < 0.01:
+            status = "Consistente"
+        elif anomalos/total >= 0.01 and anomalos/total <= 0.05:
+            status = "Atenção"
+        else:
+            status = "Inconsistente"
+
+        porcentagem = (anomalos/total)*100
+
+        f.write(f"          Dados Anômalos: {porcentagem:,.2f}%\n")
+        f.write(f"          Situação: {status}\n")
+
+        f.write("\n\n")
+
+def processa_tudo_ausente(name_project, data):
+    with open(f"summary_log.txt-{data}", 'a', encoding="utf-8") as f:
+        f.write(f"Nome do Projeto: {name_project}\n")
+        f.write(f"Data: {data}\n")
+        f.write(f"Não houve arquivos para o dia especificado.\n")
+        
+        f.write("\n\n")
+
---
+++ main_ammonit.py
@@ -0,0 +1,134 @@
+
+# %%
+#project_keys = ["GWFR", "MFXD", "DDCV", "DHMJ", "KHVF", "ZXXR", "VHKM"]
+# VHKM
+#token =  "43e0270934f859c6f09b28cdb63274beecc4aae3"
+
+import json
+import requests
+from datetime import datetime, timedelta    
+import csv
+from io import StringIO
+from ammonit_relatorio import processa_tudo, processa_tudo_ausente
+
+estacoes_chave_valor = {"GWFR": "f7c9d9394c73b9bd3d020eb485d5387de6b4d31b",
+                        "MFXD": "8d96c5fbfc6f1d61141f7c9743d157e4f836fbf7",
+                        "DDCV": "2af5f00785a4df006f4b5a93c6dca699a546f5ba", 
+                        "DHMJ": "cb9be99f7ed3767d132c34b1c1e0a484564bc5be", 
+                        "KHVF": "1a8dbdc10d927c4cd1a367114a3ebcc29d5381ee",
+                        "VHKM": "ce3c7608b5c3806db6fb48303b7a9128ac4fd6d1"}
+
+# Santana (ACESSO), Ilha de Maracá, Porto Grande (ALLOW), Laranjal Jari (ACESSO), Tartarugalzinho (ACESSO), Fernando de Noronha (ACESSO)
+
+def extrair_data(nome_arquivo):
+  componentes = nome_arquivo.split('_')
+
+  # Verificar se a lista tem pelo menos 3 elementos (nome, data, extensão)
+  if len(componentes) < 3:
+    return None
+
+  # Extrair a data do meio e formatar
+  data_str = componentes[1]
+  data = f"{data_str[6:8]}-{data_str[4:6]}-{data_str[:4]}"
+  return data
+
+
+def converter_data(data_str):
+    # Verifica se a string tem o comprimento correto
+    if len(data_str) != 8 or not data_str.isdigit():
+        raise ValueError("A string deve ter exatamente 8 dígitos no formato YYYYMMDD")
+
+    # Extrai ano, mês e dia da string
+    ano = data_str[:4]
+    mes = data_str[4:6]
+    dia = data_str[6:8]
+
+    # Formata a data no formato DD/MM/AAAA
+    data_formatada = f"{dia}-{mes}-{ano}"
+
+    return data_formatada
+
+def str_to_csv(data_str, output_file):
+        csv_data = StringIO(data_str)
+        with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
+            # Criar um objeto DictReader para ler a string como um arquivo CSV
+            reader = csv.reader(csv_data)
+            
+            # Criar um objeto writer para escrever o arquivo CSV
+            writer = csv.writer(csvfile)
+            
+            # Escrever as linhas no arquivo CSV
+            for row in reader:
+                writer.writerow(row)
+
+# Retorna string do dia anterior, isto é, 20240728
+def get_dia_anterior():
+    yesterday = datetime.now() - timedelta(2)
+    yesterday_str = yesterday.strftime('%Y%m%d')
+    return yesterday_str
+# %%
+def get_data(project_key, token):
+    url = f"https://or.ammonit.com/api/{project_key}/loggers-list/"
+    headers = {"Authorization": f"Token {token}"}
+    data = requests.get(url, headers=headers).json()
+    if len(data) > 0:
+        project_data = data[0]['project']
+        key = project_data['key']
+        name = project_data['name']
+        serial = data[0]['serial']
+        latitude = data[0]['override_latitude']
+        longitude = data[0]['override_longitude']
+
+    return key, name, serial, latitude, longitude
+# %%
+
+# Obter lista de arquivos
+# para minuto sete primary
+# para secundo sete secondary
+def get_files(project_key, token, file_type="primary"):
+    project_key, name, device_serial, latitude, longitude = get_data(project_key, token)
+    url = f"https://or.ammonit.com/api/{project_key}/{device_serial}/files/{file_type}/"
+    headers = {"Authorization": f"Token {token}"}
+    data = requests.get(url, headers=headers).json()
+
+    resultados = []
+    for i in data:
+        if i.get('is_valid'):
+            resultados.append(i.get('original_filename'))
+    return resultados
+
+
+
+#for project_key, token in estacoes_chave_valor.items():
+#project_key = "DHMJ"
+#token = "cb9be99f7ed3767d132c34b1c1e0a484564bc5be"
+if __name__=="__main__":
+
+    for project_key, token in estacoes_chave_valor.items():
+
+        headers = {"Authorization": f"Token {token}"}
+
+        lista_arquivos = get_files(project_key, token)
+
+        yesterday_files = [file for file in lista_arquivos if get_dia_anterior() in file]
+
+        # %%
+        file_type = "primary"
+
+        project_key, name, device_serial, latitude, longitude = get_data(project_key, token)
+
+        if len(yesterday_files) > 0:
+            url = f"https://or.ammonit.com/api/{project_key}/{device_serial}/files/{file_type}/{str(yesterday_files[0])}/"
+
+            arquivo = requests.get(url, headers=headers)
+            resposta = arquivo.json().get('file_content')
+
+            str_to_csv(resposta, yesterday_files[0])
+            processa_tudo(yesterday_files[0], name_project=name, data=converter_data(get_dia_anterior()))
+
+        else:
+            processa_tudo_ausente(name_project=name, data=converter_data(get_dia_anterior()))
+            print(f"Não foi encontrado dados do dia especificado para a estação: {name}")
+
+# %%
+# %%
